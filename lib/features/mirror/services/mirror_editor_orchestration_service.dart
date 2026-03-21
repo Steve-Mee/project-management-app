18e08feb02deda88404d31cb5562965ddf6ea259
Steve <steve.meerschaut@telenet.be>
2026-03-21 18:11:26 +0100

    chore(mirror): remove unnecessary orchestration override

--- lib/features/mirror/services/mirror_editor_orchestration_service.dart
+++ lib/features/mirror/services/mirror_editor_orchestration_service.dart
@@ -1,38 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../generated/app_localizations.dart';
 import 'mirror_run_flow_service.dart';
-import 'mirror_service_boundaries.dart';
 
 @Deprecated('Use MirrorRunFlowService via mirrorInteractiveRunCoordinatorProvider')
-class MirrorEditorOrchestrationService
-  extends MirrorRunFlowService
-  implements MirrorInteractiveRunCoordinator {
+class MirrorEditorOrchestrationService extends MirrorRunFlowService {
   const MirrorEditorOrchestrationService();
-
-  @override
-  Future<void> runCurrentFileInTerminal({
-    required BuildContext context,
-    required WidgetRef ref,
-    required String projectId,
-    required String taskId,
-    required String selectedMode,
-    required String sessionKey,
-    required AppLocalizations l10n,
-    required bool Function() isMounted,
-    required void Function(String line) appendTerminalLine,
-  }) async {
-    return super.runCurrentFileInTerminal(
-      context: context,
-      ref: ref,
-      projectId: projectId,
-      taskId: taskId,
-      selectedMode: selectedMode,
-      sessionKey: sessionKey,
-      l10n: l10n,
-      isMounted: isMounted,
-      appendTerminalLine: appendTerminalLine,
-    );
-  }
 }
